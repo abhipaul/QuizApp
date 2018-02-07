f0ccabc316ebf6134902e134a5a2dc36001d2f84
KITHYD\KIT912 <abhijit.paul@kony.com>
2018-02-07 12:12:42 +0530

    Fresh copy

---
+++ app/src/main/java/quickquiz/kony/com/quiz/MainActivity.java
@@ -0,0 +1,37 @@
+package quickquiz.kony.com.quiz;
+
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import quickquiz.kony.com.quiz.listener.FragmentDialogueDismiss;
+
+public class MainActivity extends AppCompatActivity implements FragmentDialogueDismiss{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void getQuestion(View v)
+    {
+        Intent i= new Intent(MainActivity.this, QuestionActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    @Override
+    public void onDismiss(boolean b) {
+
+    }
+}
---
+++ app/src/main/java/quickquiz/kony/com/quiz/listener/FragmentDialogueDismiss.java
@@ -0,0 +1,9 @@
+package quickquiz.kony.com.quiz.listener;
+
+/**
+ * Created by KIT912 on 2/6/2018.
+ */
+
+public interface FragmentDialogueDismiss {
+    public void onDismiss(boolean b);
+}
